9e39560918e7b9922e90b5f66c0539bcc8cd8e9e
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2026-09-01 21:09:18 +0530

    fixing wrong compile

--- src/sicp/chapter2.clj
+++ src/sicp/chapter2.clj
@@ -1277,6 +1277,11 @@
         (variable? exp) (if (same-variable? exp var) 1 0)
         (sum? exp) (make-sum (deriv (addend exp) var)
                              (deriv (augend exp) var))
+        (product? exp) (make-sum
+                        (make-product (multiplier exp)
+                                      (deriv (multiplicand exp) var))
+                        (make-product (deriv (multiplier exp) var)
+                                      (multiplicand exp)))
         (exponentiation? exp) (make-product
                                (make-product
                                 (exponent exp)
